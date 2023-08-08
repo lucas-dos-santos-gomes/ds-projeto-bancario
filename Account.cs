ee3b900360a428bcd8f21202cb3f50017943a31d
Lucas dos Santos Gomes <lucasdev.programador@gmail.com>
2023-08-08 02:21:30 -0300

    Finalizando projeto

---
+++ Account.cs
@@ -0,0 +1,36 @@
+namespace BankProject;
+
+class Account {
+  private double balance;
+  private int number;
+
+  public double GetBalance() {
+    return balance;
+  }
+
+  public void SetBalance(double balance) {
+    this.balance = balance;
+  } 
+
+  public int GetNumber() {
+    return number;
+  }
+
+  public void SetNumber(int number) {
+    this.number = number;
+  }
+
+  public double Deposit(double value) {
+    balance += value;
+    return GetBalance();
+  }
+
+  public bool Withdraw(double value) {
+    if(value > balance) {
+      return false;
+    } else {
+      balance -= value;
+      return true;
+    }
+  }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace BankProject;
+class Program {
+  static void Main(string[] args) {
+    string count;
+    Account conta = new();
+    Console.WriteLine("Insira o número da sua conta bancária:");
+    conta.SetNumber(int.Parse(Console.ReadLine()!));
+
+    Console.WriteLine("\nInsira o saldo da sua conta bancária:");
+    conta.SetBalance(double.Parse(Console.ReadLine()!));
+    Console.Clear();
+    do {
+      Console.WriteLine("\n=== Projeto Bancário ===");
+      Console.WriteLine("Selecione uma das opção para prosseguir:");
+      Console.WriteLine("1 – Depositar \n2 – Retirar \n3 – Consultar saldo \n0 – Sair");
+      count = Console.ReadLine()!;
+
+      Console.Clear();
+
+      if(count == "1") {
+        Console.WriteLine("\nInsira o valor do depósito:");
+        double depositValue = double.Parse(Console.ReadLine()!);
+        conta.Deposit(depositValue);
+        Console.Clear();
+        Console.WriteLine($"O depósito de R${depositValue} foi feito com sucesso!");
+      } else if(count == "2") {
+        if(conta.GetBalance() > 0) {
+          Console.WriteLine("\nInsira o valor que deseja sacar:");
+          double withdrawValue = double.Parse(Console.ReadLine()!);
+          Console.Clear();
+          Console.WriteLine(conta.Withdraw(withdrawValue) ? $"O saque de R${withdrawValue} foi feito com sucesso!" : "ERRO! Você quer sacar um valor maior do que tem na conta!");
+        } else {
+          Console.WriteLine("Sua conta não possui nenhum valor para ser sacado.");
+        }
+      } else if (count == "3") {
+        Console.WriteLine("\nSaldo atual: R$ " + conta.GetBalance());
+      } else if (count == "0") {
+        Console.WriteLine("\nPrograma finalizado.");
+      } else{
+        Console.WriteLine("\nVocê inseriu uma operação inválida!");
+      }
+    } while (count != "0");
+  }
+}
